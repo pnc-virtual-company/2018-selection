c1e57da1d9966b05701d8aeadbc634b283d6c5b4
Dakhen SOK <dakhen.sok@student.passerellesnumeriques.org>
2018-05-09 11:35:08 +0700

    [SC060] Login with database and logout refs SC060

---
+++ application/controllers/c_login.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This controller serves the user management pages and tools.
+ * @copyright  Copyright (c) 2014-2017 Benjamin BALET
+ * @license    http://opensource.org/licenses/AGPL-3.0 AGPL-3.0
+ * @link       https://github.com/bbalet/skeleton
+ * @since      0.1.0
+ */
+
+if (!defined('BASEPATH')) { exit('No direct script access allowed'); }
+
+/**
+ * This controller serves the user management pages and tools.
+ * The difference with HR Controller is that operations are technical (CRUD, etc.).
+ */
+class Users extends CI_Controller {
+
+    /**
+     * Default constructor
+     * @author Benjamin BALET <benjamin.balet@gmail.com>
+     */
+    public function __construct() {
+        parent::__construct();
+        log_message('debug', 'URI=' . $this->uri->uri_string());
+        $this->session->set_userdata('last_page', $this->uri->uri_string());
+        if($this->session->loggedIn === TRUE) {
+           // Allowed methods
+           if ($this->session->isAdmin || $this->session->isSuperAdmin) {
+             //User management is reserved to admins and super admins
+           } else {
+             redirect('errors/privileges');
+           }
+         } else {
+           redirect('connection/login');
+         }
+        $this->load->model('users_model');
+    }
+
+    /**
+     * Display the list of all users
+     * @author Benjamin BALET <benjamin.balet@gmail.com>
+     */
+    public function index() {
+        $this->load->helper('form');
+        $data['users'] = $this->users_model->getUsersAndRoles();
+        $data['title'] = 'List of users';
+        $data['activeLink'] = 'users';
+        $data['flashPartialView'] = $this->load->view('templates/flash', $data, TRUE);
+        $this->load->view('templates/header', $data);
+        $this->load->view('menu/index', $data);
+        $this->load->view('users/index', $data);
+        $this->load->view('templates/footer', $data);
+    }
+}
+
+   
