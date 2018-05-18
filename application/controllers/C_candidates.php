71bb336023efa89ab322bf3ba62656559529b32e
Dakhen SOK <dakhen.sok@student.passerellesnumeriques.org>
2018-05-18 11:08:44 +0700

    [SC085] Security on url after logout refs SC084

--- application/controllers/C_candidates.php
+++ application/controllers/C_candidates.php
@@ -2,10 +2,22 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 Class C_candidates extends CI_Controller{
- function __construct(){
-  parent:: __construct();
-  $this->load->model('candidates_model', 'm_can');
+ public function __construct() {
+         parent::__construct();
+         log_message('debug', 'URI=' . $this->uri->uri_string());
+         $this->session->set_userdata('last_page', $this->uri->uri_string());
+         if($this->session->loggedIn === TRUE) {
+            // Allowed methods
+            if ($this->session->isAdmin) {
+              //User management is reserved to admins and super admins
+            } else {
+              redirect('errors/privileges');
+          }
+      } else {
+        redirect('connection/login');
+    }
 
+  $this->load->model('candidates_model', 'm_can');
  }
  function index(){
   $this->load->view('templates/header');
--- application/controllers/Connection.php
+++ application/controllers/Connection.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Connection extends CI_Controller {
 	/**
 	 * Default constructor
@@ -10,7 +9,6 @@ class Connection extends CI_Controller {
 			parent::__construct();
 			log_message('debug', 'URI=' . $this->uri->uri_string());
 	}
-
 	/**
 	 * Login form of the application
 	 * @author Benjamin BALET <benjamin.balet@gmail.com>
@@ -24,10 +22,11 @@ class Connection extends CI_Controller {
 		if ($this->form_validation->run() === FALSE) {
 			log_message('debug', 'Let\'s display the login form');
 			$data['title'] = 'Login';
+			$data['title'] = 'Login';
 			$data['flashPartialView'] = $this->load->view('templates/flash', $data, TRUE);
-			// $this->load->view('templates/header', $data);
+			$this->load->view('templates/header', $data);
 			$this->load->view('login/login', $data);
-			// $this->load->view('templates/footer');
+			$this->load->view('templates/footer');
 		} else {
 			$this->load->model('users_model');
 			$login = $this->input->post('login');
@@ -39,7 +38,7 @@ class Connection extends CI_Controller {
 					redirect($this->session->userdata('last_page'));
 				} else {
 					log_message('debug', 'Not last_page set. Redirect to the home page');
-					redirect('c_candidates');
+					redirect('C_candidates');
 				}
 			} else {
 				log_message('error', 'Invalid credentials for user ' . $this->input->post('login'));
@@ -48,13 +47,13 @@ class Connection extends CI_Controller {
 			}
 		}
 	}
-
 	/**
 	 * Logout endpoint. Destroy the PHP session
 	 * @author Benjamin BALET <benjamin.balet@gmail.com>
 	 */
 	public function logout()
 	{
+		//Add comment to the file connection 
 		log_message('debug', 'Logout current user and redirect to the home page');
 		$this->session->sess_destroy();
 		redirect('connection/login');
