bd611d34e0ea9c05f62b4f3cd2652917cc1e4bc2
CheaChan <chea.chann99@gmail.com>
2018-05-27 15:23:22 +0700

    [SC081] Change pie chart and change color of pie chart refs SC081

--- application/controllers/C_candidates.php
+++ application/controllers/C_candidates.php
@@ -244,14 +244,12 @@ Class C_candidates extends CI_Controller{
     $fatherHealth = $this->input->post('fatherHealth');
     $fatherHealthSpecify = $this->input->post('fatherHealthSpecify');
     $fatherEdu = $this->input->post('fatherEdu');
-
     $motherAge = $this->input->post('motherAge');
     $motherOccupation = $this->input->post('motherOccupation');
     $motherSpecify = $this->input->post('motherSpecify');
     $motherhealthStatus = $this->input->post('motherhealthStatus');
     $motherHealthSpecify = $this->input->post('motherHealthSpecify');
     $motherEducation = $this->input->post('motherEducation');
-
     $numSiblings = $this->input->post('numSiblings');
     $marriedStatus = $this->input->post('marriedStatus');
     $separated = $this->input->post('separated');
@@ -277,37 +275,6 @@ Class C_candidates extends CI_Controller{
 		}
 		echo json_encode($msg);
 	}
-
- // public function selectedCandidates(){
- // 	$this->load->view('templates/header');   
- // 	$this->load->view('menu/index');   
- // 	$this->load->view('candidates/can_list');   
- // 	$this->load->view('templates/footer'); 
- // }
- // function call count all Candidates
- // public function countCandidates(){
- // 	$resultCount = $this->m_can->countCandidates();
- // 	echo json_encode($resultCount);
- // }
- // function call count selected candidates
- // public function countSelectedCandidates(){
- // 	$resultSelectedCount = $this->m_can->countSelectedCandidates();
- // 	echo json_encode($resultSelectedCount);
- // }
- // function call count provinces
- // public function countProvinces(){
- // 	$resultProvincesCount = $this->m_can->countProvinces();
- // 	echo json_encode($resultProvincesCount);
- // }
- // public function showSelected(){
- // 	$result = $this->m_can->showSelected();
- // 	echo json_encode($result);
- // }
-
- // public function export() {
- // 	$this->load->view('candidates/export');
- // }
-
 // just like to update form
 	function updateForm($id) {		// link to form update with old info
 		$this->load->model('Candidates_model');
